5e38faa028303bcbd085a8a7b54f3dfd49d5abf8
Fedya001 <rabbitera@gmail.com>
2020-10-13 23:57:26 +0300

    Remove testing files

--- libs/dir/some_local_code.py
+++
@@ -1,2 +0,0 @@
-def some_useful_function(x: int) -> int:
-    return x * x
--- libs/dir/test_public.py
+++
@@ -1,5 +0,0 @@
-from .some_local_code import some_useful_function
-
-
-def test_useful_function() -> None:
-    assert 9 == some_useful_function(3)
